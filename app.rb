b360072575ef99d85de836bd217e03d0f0a62ce8
Luciano Oliveira <luciano101@msn.com>
2017-10-07 13:53:02 -0400

    allow you to add and edit a store name

--- app.rb
+++ app.rb
@@ -11,14 +11,30 @@ end
 
 post("/add_store") do
   store = Store.new({:name => params['name']})
-  store.save()
-  # if recipe.save
-  #   redirect '/'
-  # else
-  redirect '/'
+  if store.save
+      redirect '/'
+  else
+    @error_type = store
+    erb(:errors)
+  end
 end
 
 get("/stores/:id") do
   @store = Store.find(params.fetch("id"))
   erb(:store)
 end
+
+get("/stores/:id/edit") do
+  @store = Store.find(params[:id].to_i)
+  erb(:store_edit)
+end
+
+patch("/stores/:id") do
+  name = params.fetch('name')
+  @store = Store.find(params[:id].to_i)
+  @store.update({:name => name })
+  redirect '/stores/' + @store.id.to_s
+end
+
+
+
